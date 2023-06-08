61d49f739600e855ba5e9d40f865639bd54542c6
NateCoding <86304101+rTHENET@users.noreply.github.com>
2023-06-08 22:32:12 +0900

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.8} fontSize="sm">
-      &copy; {new Date().getFullYear()} rTHENATE All Rights Reserved.
+      &copy; {new Date().getFullYear()} NateCoding All Rights Reserved.
     </Box>
   )
 }
